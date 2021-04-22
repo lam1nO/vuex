cfa83136e5a527c7f04e108d8e93192da6e4831d
Lam1n <adelka200215@gmail.com>
2021-04-22 03:44:33 +0300

    gettersKnow

---
+++ src/views/EventCreate.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>Create an Event, {{ local }}</h1>
+    <p>This event was created by {{ user.id }}</p>
+    <p>There are {{ catLength }} categories</p>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+export default {
+  computed : {
+    catLength() {
+      return this.$store.getters.catLength
+    },
+    ...mapState (['user', 'categories'])
+  }
+}
+</script>
+
+<style>
+
+</style>
