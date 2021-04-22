47964438a5babc40c7908a09139d7bf8d4c430e9
Lam1n <adelka200215@gmail.com>
2021-04-22 04:09:18 +0300

    mapGetters, another getter

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -2,18 +2,16 @@
   <div>
     <h1>Create an Event, {{ local }}</h1>
     <p>This event was created by {{ user.id }}</p>
-    <p>There are {{ catLength }} categories</p>
+    <p>{{ getEventById(2) }}</p>
   </div>
 </template>
 
 <script>
-import { mapState } from 'vuex'
+import { mapState, mapGetters } from 'vuex'
 export default {
   computed : {
-    catLength() {
-      return this.$store.getters.catLength
-    },
-    ...mapState (['user', 'categories'])
+    ...mapGetters(['getEventById']),
+    ...mapState(['user', 'categories'])
   }
 }
 </script>
